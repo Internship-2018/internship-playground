a388a1a8571426007d4779d1dcd8dbf1c265d26a
mghelas <marc.ghelas@endava.com>
2018-09-28 15:04:51 +0300

    moved click listener from bind method to onCreate method Added multiple collections for each Sweet object Changed type from Object to Sweet

--- app/src/main/java/com/mghelas/internship_playground/AllProductsActivity.java
+++ app/src/main/java/com/mghelas/internship_playground/AllProductsActivity.java
@@ -9,13 +9,14 @@ import android.support.v7.widget.RecyclerView;
 import com.mghelas.internship_playground.adapters.SweetsAdapter;
 import com.mghelas.internship_playground.sweetsfactory.Chocolate;
 import com.mghelas.internship_playground.sweetsfactory.Lollipop;
+import com.mghelas.internship_playground.sweetsfactory.Sweet;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AllProductsActivity extends Activity {
 
-    private List<Object> sweetsList = new ArrayList<>();
+    private List<Sweet> sweetsList = new ArrayList<>();
     private SweetsAdapter sweetsAdapter;
 
     @Override
@@ -38,40 +39,46 @@ public class AllProductsActivity extends Activity {
     }
 
     private void prepareChocolateData() {
-        List<String> ingredients = new ArrayList<>();
-        ingredients.add("Milk");
-        ingredients.add("Cocoa beans");
-        ingredients.add("Sugar");
+        List<String> meteoritIngredients = new ArrayList<>();
+        meteoritIngredients.add("Milk");
+        meteoritIngredients.add("Cocoa beans");
+        meteoritIngredients.add("Sugar");
+        List<String> alunelIngredients = new ArrayList<>(meteoritIngredients);
+        List<String> fistiIngredients = new ArrayList<>(meteoritIngredients);
         Chocolate chocolate = new Chocolate("Meteorit", 50.0, 10.0, true, 50);
-        chocolate.setIngredients(ingredients);
+        meteoritIngredients.add("Hazelnut");
+        chocolate.setIngredients(meteoritIngredients);
         sweetsList.add(chocolate);
-        chocolate = new Chocolate("chocolate 1", 30.0, 15.0, true, 75);
-        chocolate.setIngredients(ingredients);
+        chocolate = new Chocolate("Alunel", 30.0, 15.0, true, 75);
+        alunelIngredients.add("Walnut");
+        chocolate.setIngredients(alunelIngredients);
         sweetsList.add(chocolate);
-        chocolate = new Chocolate("chocolate 2", 20.0, 12.0, true, 25);
-        chocolate.setIngredients(ingredients);
+        chocolate = new Chocolate("Fisti", 20.0, 12.0, true, 25);
+        fistiIngredients.add("Pistachio");
+        chocolate.setIngredients(fistiIngredients);
         sweetsList.add(chocolate);
 
     }
 
     private void prepareLollipopData() {
-        List<String> ingredients = new ArrayList<>();
-        ingredients.add("Sugar");
-        Lollipop lollipop = new Lollipop("Lollipop 1", 5.0, 10.0, false, "Apple");
-        ingredients.add(lollipop.getFlavour() + " concentrate");
-        lollipop.setIngredients(ingredients);
-        sweetsList.add(lollipop);
-        ingredients.remove(lollipop.getFlavour());
-        lollipop = new Lollipop("Lollipop 2", 7.0, 5.0, false, "Banana");
-        ingredients.add(lollipop.getFlavour() + " concentrate");
-        lollipop.setIngredients(ingredients);
-        sweetsList.add(lollipop);
-        ingredients.remove(lollipop.getFlavour());
-        lollipop = new Lollipop("Lollipop 3", 8.0, 11.0, false, "Strawberry");
-        ingredients.add(lollipop.getFlavour() + " concentrate");
-        lollipop.setIngredients(ingredients);
-        sweetsList.add(lollipop);
+        List<String> curcubeuIngredients = new ArrayList<>();
+        curcubeuIngredients.add("Sugar");
+        List<String> jellyIngredients = new ArrayList<>(curcubeuIngredients);
+        List<String> faniIngredients = new ArrayList<>(curcubeuIngredients);
 
+        List<String> ingredients = new ArrayList<>();
+        Lollipop lollipop = new Lollipop("Curcubeu", 5.0, 10.0, false, "Apple");
+        curcubeuIngredients.add(lollipop.getFlavour() + " concentrate");
+        lollipop.setIngredients(curcubeuIngredients);
+        sweetsList.add(lollipop);
+        lollipop = new Lollipop("Jelly", 7.0, 5.0, false, "Banana");
+        jellyIngredients.add(lollipop.getFlavour() + " concentrate");
+        lollipop.setIngredients(jellyIngredients);
+        sweetsList.add(lollipop);
+        lollipop = new Lollipop("Fani", 8.0, 11.0, false, "Strawberry");
+        faniIngredients.add(lollipop.getFlavour() + " concentrate");
+        lollipop.setIngredients(faniIngredients);
+        sweetsList.add(lollipop);
     }
 
 }
--- app/src/main/java/com/mghelas/internship_playground/adapters/SweetsAdapter.java
+++ app/src/main/java/com/mghelas/internship_playground/adapters/SweetsAdapter.java
@@ -11,91 +11,97 @@ import android.widget.Toast;
 import com.mghelas.internship_playground.R;
 import com.mghelas.internship_playground.sweetsfactory.Chocolate;
 import com.mghelas.internship_playground.sweetsfactory.Lollipop;
+import com.mghelas.internship_playground.sweetsfactory.Sweet;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private List<Object> items = new ArrayList<>();
+    private List<Sweet> items = new ArrayList<>();
     private static final int ITEM_TYPE_CHOCOLATE = 1;
     private static final int ITEM_TYPE_LOLLIPOP = 2;
 
-    public SweetsAdapter(List<Object> items) {
-        System.out.println("asdasd");
+    public SweetsAdapter(List<Sweet> items) {
         this.items.addAll(items);
     }
 
-    private static class ChocolateViewHolder extends RecyclerView.ViewHolder {
+    public List<Sweet> getItems() {
+        return items;
+    }
 
-        public TextView title, price, weight, percentage;
-        public Button manufacture;
+    private class ChocolateViewHolder extends RecyclerView.ViewHolder {
 
-        public ChocolateViewHolder(View itemView) {
+        private TextView title;
+        private TextView price;
+        private TextView weight;
+        private TextView percentage;
+        private Button manufacture;
+
+        ChocolateViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
             price = (TextView) itemView.findViewById(R.id.price);
             weight = (TextView) itemView.findViewById(R.id.weight);
             percentage = (TextView) itemView.findViewById(R.id.percentage);
             manufacture = (Button) itemView.findViewById(R.id.manufacture);
-
-            // prepare your ViewHolder
-        }
-
-        public void bind(final Chocolate chocolate) {
-            title.setText(chocolate.getTitle());
-            price.setText(chocolate.getPrice() + "");
-            weight.setText(chocolate.getWeight() + "");
-            percentage.setText(chocolate.getPercentage() + "");
-
+//            System.out.println(get);
             manufacture.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast toast = Toast.makeText(v.getContext(),
-                            chocolate.manufacture(),
+                            getItems().get(getAdapterPosition()).manufacture(),
                             Toast.LENGTH_SHORT);
 
                     toast.show();
                 }
             });
         }
+
+        void bind(final Chocolate chocolate) {
+            title.setText(chocolate.getTitle());
+            price.setText(chocolate.getPrice() + "");
+            weight.setText(chocolate.getWeight() + "");
+            percentage.setText(chocolate.getPercentage() + "");
+        }
     }
 
-    private static class LollipopViewHolder extends RecyclerView.ViewHolder {
+    private class LollipopViewHolder extends RecyclerView.ViewHolder {
         public TextView title, price, weight, flavour;
         public Button manufacture;
 
-        public LollipopViewHolder(View itemView) {
+        LollipopViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
             price = (TextView) itemView.findViewById(R.id.price);
             weight = (TextView) itemView.findViewById(R.id.weight);
             flavour = (TextView) itemView.findViewById(R.id.flavour);
             manufacture = (Button) itemView.findViewById(R.id.manufacture);
-        }
-
-        public void bind(final Lollipop lollipop) {
-            title.setText(lollipop.getTitle());
-            price.setText(lollipop.getPrice() + "");
-            weight.setText(lollipop.getWeight() + "");
-            flavour.setText(lollipop.getFlavour() + "");
 
             manufacture.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast toast = Toast.makeText(v.getContext(),
-                            lollipop.manufacture(),
+                            getItems().get(getAdapterPosition()).manufacture(),
                             Toast.LENGTH_SHORT);
 
                     toast.show();
                 }
             });
         }
+
+        void bind(final Lollipop lollipop) {
+            title.setText(lollipop.getTitle());
+            price.setText(lollipop.getPrice() + "");
+            weight.setText(lollipop.getWeight() + "");
+            flavour.setText(lollipop.getFlavour() + "");
+        }
     }
 
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        System.out.println(viewType + " asd");
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         if (viewType == ITEM_TYPE_CHOCOLATE) {
             View v = layoutInflater.inflate(R.layout.chocolate_list_row, parent, false);
@@ -110,7 +116,7 @@ public class SweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-        Object item = items.get(position);
+        Sweet item = items.get(position);
 
         if (viewHolder instanceof ChocolateViewHolder) {
             ((ChocolateViewHolder) viewHolder).bind((Chocolate) item);
@@ -132,5 +138,7 @@ public class SweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public int getItemCount() {
         return this.items.size();
     }
+
+
 
 }
