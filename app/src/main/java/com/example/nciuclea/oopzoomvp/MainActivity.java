6a7346b6d57c2a3b4d6cd2ff831a7eb6ba3465a2
ENDAVA\nciuclea <nikita.ciuclea@gmail.com>
2018-09-28 15:56:09 +0300

    Moved fragment replacing in replaceFragment();

--- app/src/main/java/com/example/nciuclea/oopzoomvp/MainActivity.java
+++ app/src/main/java/com/example/nciuclea/oopzoomvp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nciuclea.oopzoomvp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(findViewById(R.id.welcome_container) != null) {
-            WelcomeFragment welcomeFragment = new WelcomeFragment();
+        Fragment welcomeFragment = new WelcomeFragment();
+
+        replaceFragment(R.id.fragment_container, welcomeFragment);
+    }
+
+    private void replaceFragment(int containerId, Fragment fragment) {
+        if(findViewById(containerId) != null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.welcome_container, welcomeFragment)
+                    .replace(containerId, fragment)
                     .commit();
         }
     }
