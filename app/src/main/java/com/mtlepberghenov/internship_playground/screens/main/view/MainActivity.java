eaa081c6dc4821770f5536df5652066aee7db11c
mtlepberghenov <marat.tlepberghenov@endava.com>
2018-10-01 14:06:57 +0300

    Installed Crashlytics

--- app/src/main/java/com/mtlepberghenov/internship_playground/screens/main/view/MainActivity.java
+++ app/src/main/java/com/mtlepberghenov/internship_playground/screens/main/view/MainActivity.java
@@ -3,14 +3,17 @@ package com.mtlepberghenov.internship_playground.screens.main.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.mtlepberghenov.internship_playground.R;
 import com.mtlepberghenov.internship_playground.screens.welcome.WelcomeFragment;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         startWelcomeFragment();
