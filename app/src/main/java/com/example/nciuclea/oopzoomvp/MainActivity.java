f44a7891deeb32fd1f9f533bf15316b1a6f894f8
ENDAVA\nciuclea <nikita.ciuclea@gmail.com>
2018-10-02 15:57:45 +0300

    Added Crashlytics

--- app/src/main/java/com/example/nciuclea/oopzoomvp/MainActivity.java
+++ app/src/main/java/com/example/nciuclea/oopzoomvp/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nciuclea.oopzoomvp.WelcomeScreen.WelcomeFragment;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         startFragment(R.id.fragment_container, new WelcomeFragment());
