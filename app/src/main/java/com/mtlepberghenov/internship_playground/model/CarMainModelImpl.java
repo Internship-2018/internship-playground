12cf7eb41b0cc9496e2ea88537018ca29c742fdb
mtlepberghenov <marat.tlepberghenov@endava.com>
2018-10-04 17:05:25 +0300

    Deleted oldest methods

--- app/src/main/java/com/mtlepberghenov/internship_playground/model/CarMainModelImpl.java
+++ app/src/main/java/com/mtlepberghenov/internship_playground/model/CarMainModelImpl.java
@@ -6,20 +6,9 @@ import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarMainModelImpl implements MainModel {
-
-  private ArrayList<Vehicle> carList;
-
-  public CarMainModelImpl() {
-    this.carList = SingletonCarList.newInstance().getList();
-  }
-
-  @Override public ArrayList<Vehicle> getVehicleList() {
-    return this.carList;
-  }
 
   @Override public Single<List<Vehicle>> getData() {
     return Observable
